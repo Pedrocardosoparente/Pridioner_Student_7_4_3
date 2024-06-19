2137124f4a674bbfd2bf512b4c6741198286d90a
Aluno <Aluno@10.85.198.36>
2024-06-19 16:31:49 -0300

    PRI

---
+++ src/pridioner_student_7_4_3/Pridioner_Student_7_4_3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pridioner_student_7_4_3;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Pridioner_Student_7_4_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
